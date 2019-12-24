391f566d8de20ff651400a5119c2583342a3b640
Lukmannudin <laodelukmannudinpriatna@gmail.com>
2019-12-24 20:55:00 +0700

    coy

---
+++ latihan/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello <b> World </b>');
+    res.end("Hi, selamat datang di nodejs");
+}).listen(8000);
+
+console.log("Server running..")
